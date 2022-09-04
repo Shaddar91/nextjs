ab194eb4ec2697d850e0b5b6002e58ade1245ac0
My Name <shaddar45@gmail.com>
2022-09-04 21:44:35 +0200

    testing

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps }
+    />
 }
 
 export default MyApp
+
+const zec = "KEC";
+console.log(zec);
