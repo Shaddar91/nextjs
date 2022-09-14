8c0331a3fa7c34afc03f28596612541b42275f95
My Name <shaddar45@gmail.com>
2022-09-14 22:17:25 +0200

    testing

--- pages/index.js
+++ pages/index.js
@@ -21,14 +21,14 @@ export default function Home() {
         main className = { styles.main } >
         <
         h1 className = { styles.title } >
-        Welcome to < a href = "https://nextjs.org" > Next.js! < /a> < /
-        h1 >
+        Welcome to < a href = "https://nextjs.org" > Next.js! < /a> <
+        /h1>
 
         <
         p className = { styles.description } >
         Get started by editing { ' ' } <
-        code className = { styles.code } > pages / index.js < /code> < /
-        p >
+        code className = { styles.code } > pages / index.js < /code> <
+        /p>
 
         <
         div className = { styles.grid } >
@@ -37,24 +37,24 @@ export default function Home() {
         className = { styles.card } >
         <
         h2 > Documentation & rarr; < /h2> <
-        p > Find in -depth information about Next.js features and API. < /p> < /
-        a >
+        p > Find in -depth information about Next.js features and API. < /p> <
+        /a>
 
         <
         a href = "https://nextjs.org/learn"
         className = { styles.card } >
         <
         h2 > Learn & rarr; < /h2> <
-        p > Learn about Next.js in an interactive course with quizzes! < /p> < /
-        a >
+        p > Learn about Next.js in an interactive course with quizzes! < /p> <
+        /a>
 
         <
         a href = "https://github.com/vercel/next.js/tree/master/examples"
         className = { styles.card } >
         <
         h2 > Examples & rarr; < /h2> <
-        p > Discover and deploy boilerplate example Next.js projects. < /p> < /
-        a >
+        p > Discover and deploy boilerplate example Next.js projects. < /p> <
+        /a>
 
         <
         a href = "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -63,10 +63,10 @@ export default function Home() {
         h2 > Deploy & rarr; < /h2> <
         p >
         Instantly deploy your Next.js site to a public URL with Vercel. <
-        /p> < /
-        a > <
-        /div> < /
-        main >
+        /p> <
+        /a> <
+        /div> <
+        /main>
 
         <
         footer className = { styles.footer } >
@@ -81,10 +81,10 @@ export default function Home() {
         alt = "Vercel Logo"
         width = { 72 }
         height = { 16 }
-        /> < /
-        span > <
-        /a> < /
-        footer > <
+        /> <
+        /span> <
+        /a> <
+        /footer> <
         /div>
     )
 }
